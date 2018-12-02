83b211b232693a08fdc0499cf693adad019e23f5
CoderGx <386348903@qq.com>
2018-12-02 10:30:38 +0800

    input引用基类

--- GxVue/Input.jsx
+++ GxVue/Input.jsx
@@ -2,134 +2,126 @@
 /// <reference path="../Script/Gx.Base.js" />
 /// <reference path="../Lib/Vue/vue2.5.16.js" />
 (function (win) {
-    var Default = Vue.extend({
-        render: function (h) {
-            var input = <input
-                type={this.type}
-                placeholder={this.placeholder}
-                onChange={this._baseChange}
-                onBlur={this._baseOnBlur}
-                onFocus={this._baseOnFocus}
-                value={this._isFocus ? this.value : this.text}
-            />;
-            switch (this.type) {
-                case "textarea":
-                    input.tag = "textarea";
-                    break;
-                case "number":
-                    input.data.attrs.type = "text";
-                    break;
-                default:
-                    break;
-            }
-            return (input);
-        },
-        //计算属性
-        computed: {
-            text: {
-                cache: false,
-                get: function () {
-                    var num = this.value;
-                    switch (this.type) {
-                        case "number":
+
+    var optionObj = {};
+    optionObj.render = function (h) {
+        var input = <input
+            class={[
+                "form-control"
+            ]}
+            style={{
+                "width": this.width
+            }}
+            type={this.type}
+            placeholder={this.placeholder}
+            onChange={this._baseChange}
+            onBlur={this._baseOnBlur}
+            onFocus={this._baseOnFocus}
+            value={this.$data._isFocus ? this.value : this.text}
+        />;
+        switch (this.type) {
+            case "textarea":
+                input.tag = "textarea";
+                break;
+            case "number":
+                input.data.attrs.type = "text";
+                break;
+            default:
+                break;
+        }
+        return (input);
+    };
+    optionObj.computed = {
+        text: {
+            cache: false,
+            get: function () {
+                var num = this.value;
+                switch (this.type) {
+                    case "number":
+                        if (num) {
                             num = Gx.convert.toNumber(num, true);
-                            break;
-                        default:
-                            break;
-                    }
-                    return num;
+                        }
+                        break;
+                    default:
+                        break;
                 }
-            }
-        },
-        //侦听属性
-        watch: {
-        },
-        //方法
-        methods: {
-            _baseChange: function () {
-                this.value = this.$el.value;
-                this.change();
-            },
-            //失焦焦点
-            _baseOnBlur: function () {
-                this._isFocus = false;
-                this.onBlur();
-                //验证
-                if (this.validation(this.value)) {
-
-                }
-            },
-            //获得焦点
-            _baseOnFocus: function () {
-                this._isFocus = true;
-
-                this.onFocus();
-            }
-        },
-        data: function () {
-            return {
-            };
-        },
-        props: {
-            type: {
-                "default": "text"
-            },
-            //提示
-            placeholder: {
-                "default": ""
-            },
-            //验证方法
-            validation: {
-                "default": function () {
-                    return function (value) {
-                        return true;
-                    };
-                }
-            },
-            //change事件
-            change: {
-                "default": function () {
-                    return function () {
-                    };
-                }
-            },
-            //失焦焦点
-            onBlur: {
-                "default": function () {
-                    return function () {
-                    };
-                }
-            },
-            //获得焦点
-            onFocus: {
-                "default": function () {
-                    return function () {
-                    };
-                }
-            },
-            value: {
-                "default": ""
-            },
-            _isFocus: {
-                "default": true
+                return num;
             }
         }
-    });
+    };
+    optionObj.methods = {
+        _baseChange: function () {
+            this.value = this.$el.value;
+            this.change();
+        },
+        //失焦焦点
+        _baseOnBlur: function () {
+            this.$data._isFocus = false;
+            this.onBlur();
+            //验证
+            if (this.validation(this.value)) {
+
+            }
+        },
+        //获得焦点
+        _baseOnFocus: function () {
+            this.$data._isFocus = true;
+
+            this.onFocus();
+        }
+    };
+    optionObj.data = function () {
+        return {
+            _isFocus: false
+        };
+    };
+    optionObj.props = {
+        type: {
+            "default": "text"
+        },
+        //提示
+        placeholder: {
+            "default": ""
+        },
+        //验证方法
+        validation: {
+            "default": function () {
+                return function (value) {
+                    return true;
+                };
+            }
+        },
+        //change事件
+        change: {
+            "default": function () {
+                return function () {
+                };
+            }
+        },
+        //失焦焦点
+        onBlur: {
+            "default": function () {
+                return function () {
+                };
+            }
+        },
+        //获得焦点
+        onFocus: {
+            "default": function () {
+                return function () {
+                };
+            }
+        },
+        value: {
+            "default": ""
+        },
+
+    };
+    var Default = Vue.extend(Gx.base.mergeParam(Gx.ui.getDefaultObj(), optionObj));
 
     Gx.ui.coms.Input = Default;
 
     Gx.ui.createInput = function () {
-
-        var paramObj = Gx.param.getSerializeParam(arguments);
-
-        var option = new Default({ propsData: paramObj.obj[0] });
-        if (paramObj.str[0]) {
-            option = option.$mount("#" + paramObj.str[0]);
-        }
-        else {
-            option = option.$mount();
-        }
-
-        return option;
+        return Gx.ui.createInstance(Default, Gx.param.getSerializeParam(arguments));
     };
 })(window);
