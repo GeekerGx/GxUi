01a69846c2fc0a7e1b9fae32b86a049038cd12de
CoderGx <386348903@qq.com>
2018-12-05 22:01:42 +0800

    修改编码格式

--- GxVue/Default.jsx
+++ GxVue/Default.jsx
@@ -3,55 +3,13 @@
 /// <reference path="../Lib/Vue/vue2.5.16.js" />
 
 (function (win) {
-    
-    var Default = Vue.extend({
-        render: function (h) {
-            return (
-                <div>{this.value}</div>
-            );
-        },
-        //计算属性
-        computed: {
-        },
-        //侦听属性
-        watch: {
-        },
-        //方法
-        methods: {
-        },
-        data: function () {
-            return {
-            };
-        },
-        beforeCreate: function () {
-        },
-        created: function () {
-        },
-        beforeMount: function () {
-        },
-        mounted: function () {
-        },
-        props: {
-            value: {
-                "default": "default"
-            }
-        }
-    });
+
+    var optionObj = {};
+    var Default = Vue.extend(Gx.base.mergeParam(Gx.ui.getDefaultObj(), optionObj));
 
     Gx.ui.coms.Default = Default;
 
     Gx.ui.createDefault = function () {
-
-        var paramObj = Gx.param.getSerializeParam(arguments);
-
-        var option = new Default({ propsData: paramObj.obj[0] });
-        if (paramObj.str[0]) {
-            option = option.$mount("#" + paramObj.str[0]);
-        }
-        else {
-            option = option.$mount();
-        }
-
-        return option;
+        return Gx.ui.createInstance(Default, Gx.param.getSerializeParam(arguments));
     };
 })(window);
