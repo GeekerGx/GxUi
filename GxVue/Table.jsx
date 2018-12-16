b4f531d1741dbbace98bac5f0610d05922a760ab
CoderGx <386348903@qq.com>
2018-12-16 22:29:36 +0800

    准备封装table

--- GxVue/Table.jsx
+++ GxVue/Table.jsx
@@ -8,25 +8,47 @@
         return {
             undefinedText: '-',//数据为空时显示字符串
             striped: true,//隔行变色效果
-            columns:[],//列设置
-            data:[],//数据
-            pagination:true,//分页
-            paginationLoop:false,//循环分页
-            pageNumber:1,//页码
-            pageSize:10,//每页数据条数
-            pageList:[10,50,100],//每页数据条数下拉
-            smartDisplay:false,//判断显示分页信息和 card 视图
-            search:false,//搜索框
-            showPaginationSwitch:true,//切换分页按钮
-            singleSelect:true,//单选
-            toolbar:undefined,//toolbar位置，jq选择器
-            buttonsToolbar:undefined,//buttonsToolbar位置，jq选择器
+            columns: [],//列设置
+            data: [],//数据
+            pagination: true,//分页
+            paginationLoop: false,//循环分页
+            pageNumber: 1,//页码
+            pageSize: 10,//每页数据条数
+            pageList: [10, 50, 100],//每页数据条数下拉
+            smartDisplay: false,//判断显示分页信息和 card 视图
+            search: false,//搜索框
+            showPaginationSwitch: true,//切换分页按钮
+            singleSelect: true,//单选
+            toolbar: undefined,//toolbar位置，jq选择器
+            buttonsToolbar: undefined,//buttonsToolbar位置，jq选择器
+            onClickRow: function (row, $el) { },//单击行
+            onDblClickRow: function (row, $el) { },//双击行
+        };
+    };
+    var _columnSetting = function () {
+        return {
+            checkbox: false,//复选框
+            field: "",
+            title: "",
+            titleTooltip: "",
+            halign: "center",
+            align: "center",
+            width: 200,
+            visible: true,
+            formatter: function (value, row, index) { },
+
         };
     };
     var optionObj = {};
+    optionObj.render = function (h) {
+        return (
+            <table ref="table"></table>
+        );
+    };
+    
+
     var Default = Vue.extend(Gx.base.mergeParam(Gx.ui.getDefaultObj(), optionObj));
     Gx.ui.coms.Table = Default;
-
     Gx.ui.createTable = function () {
         return Gx.ui.createInstance(Default, Gx.param.getSerializeParam(arguments));
     };
